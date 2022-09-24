4c4f725838646f1abd102f8632051a0dc0a34007
Paulo Moutinho <paulo_vicali@icloud.com>
2022-09-24 19:57:47 -0300

    async/await com fecth e axios

--- main.js
+++ main.js
@@ -21,6 +21,7 @@ showApi().then(repo => {
   console.log(err)
 })
 
+// PROMISE ALL
 // Executando Promessas em paralelo com Promise All (duas promessas sendo executada juntas)
 Promise.all([
   axios.get("https://api.github.com/users/moutinhofuturedev"),
@@ -34,3 +35,48 @@ Promise.all([
   console.log('Finalizou!')
 })
 
+// ASYNC/AWAIT
+// => uma maneira de escrever promises
+// => syntactic sugar: trocar then por await         
+
+const aPromise = new Promise((resolve, reject) => {
+  return resolve('ok')
+})
+
+async function start() {
+  const result = await aPromise
+  console.log(`Nova promessa ${result}`)
+}
+
+start()
+
+// ASYNC/AWAIT
+// => exemplo com fatch
+
+// fetch('https://api.github.com/users/moutinhofuturedev')
+// .then(response => response.json())
+// .then(data => fetch(data.repos_url))
+// .then(res => res.json())
+// .then(d => console.log(d))  => desse jeito funciona mas não é o adequado
+
+// maneira melhor de escrever uma promoses  
+async function starting() {
+  const response = await fetch('https://api.github.com/users/moutinhofuturedev').then(res => res.json())
+  const reposResponse = await fetch(response.repos_url).then(repo => repo.json())
+
+  return console.log(reposResponse)
+}
+
+starting()
+
+// ASYNC/AWAIT
+// => exemplo com Axios
+
+async function fecthRepos() {
+  const user = await axios.get('https://api.github.com/users/moutinhofuturedev')
+  const repos = await axios.get(user.data.repos_url)
+
+  return console.log(repos)
+}
+
+fecthRepos()
